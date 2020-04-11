0dee95ccc09af7c761930fe6b61f7876ac4eea41
joaquin100 <is708601@iteso.mx>
2020-04-10 19:57:10 -0500

    iniciando formularios

--- src/app/productos/product-main/product-edit/product-edit.component.ts
+++ src/app/productos/product-main/product-edit/product-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router,Params, ActivatedRoute } from '@angular/router';
 import {Location} from '@angular/common';
+import { Product, Especificacion } from '../../Product';
 
 
 @Component({
@@ -9,13 +10,20 @@ import {Location} from '@angular/common';
   styleUrls: ['./product-edit.component.scss']
 })
 export class ProductEditComponent implements OnInit {
-
+  modoAgregar = true;//modo /products/new
   id: number;
+  marcas = ["HP","Sony","LG"];
+  especificaciones = [];
+  product:Product;
+
+  especificacionesIndex = 0;
+
   constructor(private router: Router, private route: ActivatedRoute, private location: Location) {
 
   }
 
   ngOnInit() {
+    this.product = new Product(0,"","","",0,0,[new Especificacion("","","")]);
     this.route.params.subscribe((params) => this.id = params['id'])
   }
 
@@ -27,6 +35,12 @@ export class ProductEditComponent implements OnInit {
     this.router.navigate(['/products']);
     //Otra forma para regresar a la ruta anterior
     this.location.back();
+  }
+
+  agregarEspecificacion(){
+    console.log("Agregando especificación");
+    this.product.especificacion.push(new Especificacion("","",""));
+    this.especificacionesIndex++;
   }
 
 
